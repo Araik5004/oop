c21ccfc1d53f1792d6ee37cc05d283d61c7b2e4e
Araik <araik5004@mail.ru>
2025-08-22 16:29:05 +0400

    add adapter files

--- public/adapter.php
+++ public/adapter.php
@@ -1,12 +1,9 @@
 <?php
 
-require_once dirname(__DIR__, 1).'/vendor/autoload.php';
+require_once dirname(__DIR__, 1) . '/vendor/autoload.php';
 
 $newCsvFileWriter = new \App\NewCsvFileWriter();
-$filterWriter = new \App\NewFilterWriterAdapter($newCsvFileWriter);
+$fileWriter = new \App\NewFileWriterAdapter($newCsvFileWriter);
 
-$processor = new \App\RandomProcessor($filterWriter);
+$processor = new \App\RandomProcessor($fileWriter);
 $processor->process(['foo' => 'bar']);
-
-
-
